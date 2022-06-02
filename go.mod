f98368d78a11b9f322b8ae519a5033aaa815fd2b
Dominik Menke <dom@digineo.de>
2022-06-02 21:58:25 +0200

    tex: add more tests

--- go.mod
+++ go.mod
@@ -48,7 +48,7 @@ require (
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/goleak v1.1.12 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
-	golang.org/x/net v0.0.0-20220526153639-5463443f8c37 // indirect
+	golang.org/x/net v0.0.0-20220531201128-c960675eff93 // indirect
 	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/time v0.0.0-20210723032227-1f47c861a9ac // indirect
--- tex/document_test.go
+++ tex/document_test.go
@@ -1,14 +1,98 @@
 package tex
 
 import (
+	"bytes"
+	"io"
+	"io/fs"
+	"os"
 	"path"
+	"strings"
+	"sync"
 	"testing"
 
 	"github.com/spf13/afero"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
 )
+
+func TestFile_flags(t *testing.T) {
+	t.Parallel()
+	assert := assert.New(t)
+
+	subject := File{"", 0, 0}
+	assert.False(subject.isCandidate())
+	assert.False(subject.hasDocumentClass())
+	assert.False(subject.hasTexdMark())
+
+	subject.flags = flagCandidate
+	assert.True(subject.isCandidate())
+	assert.False(subject.hasDocumentClass())
+	assert.False(subject.hasTexdMark())
+
+	subject.flags = flagCandidate | flagTexdMark
+	assert.True(subject.isCandidate())
+	assert.False(subject.hasDocumentClass())
+	assert.True(subject.hasTexdMark())
+
+	// flagTexdMark XOR flagDocumentClass in practice
+	subject.flags = flagCandidate | flagDocumentClass
+	assert.True(subject.isCandidate())
+	assert.True(subject.hasDocumentClass())
+	assert.False(subject.hasTexdMark())
+}
+
+type nopCloser struct{ io.Writer }
+
+func (*nopCloser) Close() error { return nil }
+
+func testFileWriter(t *testing.T, s string, candidate bool) {
+	t.Helper()
+
+	var buf bytes.Buffer
+	f := &File{}
+	if candidate {
+		f.flags = flagCandidate
+	}
+
+	subject := fileWriter{
+		log:  zap.NewNop(),
+		file: f,
+		wc:   &nopCloser{Writer: &buf},
+		buf:  make([]byte, 4),
+	}
+
+	n, err := subject.Write([]byte(s))
+	require.NoError(t, err)
+	assert.Equal(t, len(s), n)
+	assert.EqualValues(t, s, buf.String())
+
+	if candidate {
+		offset := len(s)
+		if max := len(subject.buf); offset > max {
+			offset = max
+		}
+		assert.Equal(t, offset, subject.off)
+	} else {
+		assert.Equal(t, 0, subject.off)
+	}
+}
+
+func TestFileWriter_nonCandicate(t *testing.T) {
+	t.Parallel()
+	testFileWriter(t, "Hello World!", false)
+}
+
+func TestFileWriter_candicateSmall(t *testing.T) {
+	t.Parallel()
+	testFileWriter(t, "Hi!", true)
+}
+
+func TestFileWriter_candicateLarge(t *testing.T) {
+	t.Parallel()
+	testFileWriter(t, strings.Repeat("a", 100), true)
+}
 
 func TestCleanpath(t *testing.T) {
 	t.Parallel()
@@ -187,32 +271,6 @@ func TestDocument(t *testing.T) {
 	require.False(subject.exists("foo.tex"))
 }
 
-func TestFile_flags(t *testing.T) {
-	t.Parallel()
-	assert := assert.New(t)
-
-	subject := File{"", 0, 0}
-	assert.False(subject.isCandidate())
-	assert.False(subject.hasDocumentClass())
-	assert.False(subject.hasTexdMark())
-
-	subject.flags = flagCandidate
-	assert.True(subject.isCandidate())
-	assert.False(subject.hasDocumentClass())
-	assert.False(subject.hasTexdMark())
-
-	subject.flags = flagCandidate | flagTexdMark
-	assert.True(subject.isCandidate())
-	assert.False(subject.hasDocumentClass())
-	assert.True(subject.hasTexdMark())
-
-	// flagTexdMark XOR flagDocumentClass in practice
-	subject.flags = flagCandidate | flagDocumentClass
-	assert.True(subject.isCandidate())
-	assert.True(subject.hasDocumentClass())
-	assert.False(subject.hasTexdMark())
-}
-
 func TestDocument_MainInput(t *testing.T) {
 	t.Parallel()
 	assert := assert.New(t)
@@ -269,4 +327,50 @@ func TestDocument_MainInput(t *testing.T) {
 		_, err = subject.MainInput()
 		assert.EqualError(err, "cannot determine main input file: multiple files with %!texd mark")
 	}
+}
+
+func TestNewDocument(t *testing.T) {
+	t.Parallel()
+
+	engine := NewEngine("foo")
+	image := "bar"
+
+	subject := NewDocument(zap.NewNop(), engine, image)
+	require.NotNil(t, subject)
+
+	assert.Equal(t, engine, subject.Engine())
+	assert.Equal(t, image, subject.Image())
+}
+
+type mockFs struct {
+	mock.Mock
+	afero.Fs
+}
+
+func (fs *mockFs) Mkdir(name string, mode fs.FileMode) error {
+	args := fs.Called(name, mode)
+	return args.Error(0)
+}
+
+func TestDocument_WorkingDirectory(t *testing.T) {
+	t.Parallel()
+
+	mockfs := &mockFs{}
+	mockfs.On("Mkdir", mock.AnythingOfType("string"), fs.FileMode(0o700)).
+		Return(os.ErrPermission).
+		Times(1)
+
+	subject := document{
+		fs:        mockfs,
+		mkWorkDir: &sync.Once{},
+	}
+
+	wd, err := subject.WorkingDirectory()
+	require.EqualError(t, err, "creating working directory failed: permission denied")
+	assert.Equal(t, wd, "")
+
+	// mkWorkDir is not called again, otherwise we'll see a
+	// "mock: The method has been called over 1 times" failure
+	_, err = subject.WorkingDirectory()
+	require.Error(t, err)
 }
